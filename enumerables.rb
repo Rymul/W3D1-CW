17d8b5032dc23ffd0ebcaaf49793d3757a522aeb
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-06-27 15:15:01 -0700

    third commit anna's first commit

--- enumerables.rb
+++ enumerables.rb
@@ -2,20 +2,31 @@ require "byebug"
 class Array
     def my_each(&prc)
         i = 0
-        # debugger
+      arr =[]
         while i < self.length
-            self[i] = prc.call(self[i])
+            arr << prc.call(self[i])    
             i += 1
         end
-        self
+        arr
+    end
+
+    def my_select(&blk)    
+           i = 0
+           arr =[]
+           bools = self.my_each(&blk)
+           while i < self.length                            #  [true, true, false false, true] [1,2,3,4,5]
+               if bools[i] == true
+                    arr << self[i]
+                end
+                i += 1
+           end
+           arr
     end
 
 
 
 end
 
-
-
 arr = [1,2,3]
-p arr.my_each {|ele| ele * 2}
+p arr.my_select {|ele| ele % 2 != 0}
 
